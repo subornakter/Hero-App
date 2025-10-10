f2d33c304b9e6bccf9031db84d4c3086377309ef
subornakter <subornakter01@gmail.com>
2025-10-10 14:36:40 +0600

    add

--- src/Hooks/useApps.js
+++ src/Hooks/useApps.js
@@ -8,7 +8,7 @@ const useApps = () => {
 
   useEffect(() => {
     setLoading(true)
-    axios('../AppData.json')
+    axios('/AppData.json')
       .then(data => setApps(data.data))
       .catch(err => setError(err))
       .finally(() => setLoading(false))
